b942a044df27377900624caaaf1149cb27f3c599
Austin Hasten <16011612+AustinHasten@users.noreply.github.com>
2021-03-16 11:47:09 -0500

    Initial commit
    Initial commit

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from aqt.qt import QFontDatabase
+from aqt import gui_hooks
+import random
+
+from aqt import gui_hooks
+def prepare(html, card, context):
+    qf = QFontDatabase()
+    thefont = random.choice(qf.families(QFontDatabase.Japanese))
+    return html + '''
+    <script>
+        el = document.getElementsByClassName("randomfont")
+        for(var i = 0; i < el.length; i++) {
+            el[i].style.fontFamily = "''' + thefont + '''";
+        }
+    </script>
+    '''
+    
+gui_hooks.card_will_show.append(prepare)
